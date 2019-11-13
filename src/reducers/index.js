1b1c35b101bf050eb89de6f96007a05371cdfac3
Barrierok <ilyazubtsov@gmail.com>
2019-11-13 08:42:39 +0300

    fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
+import { combineReducers } from 'redux-starter-kit';
 import channels from './channels';
 import messages from './messages';
 import states from './actionState';
@@ -8,5 +7,4 @@ export default combineReducers({
   channels,
   messages,
   ...states,
-  form: formReducer,
 });
--- src/utils/reduxForm.js
+++
@@ -1,3 +0,0 @@
-import { reduxForm } from 'redux-form';
-
-export default name => Component => reduxForm({ form: `${name}`, enableReinitialize: true })(Component);
