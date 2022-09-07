cf6c4311cd9786e97c9d8678fa18406b46b23e0b
ilya Zubtsov <i.zubtsov@pik-remont.ru>
2022-09-07 22:19:37 +0300

    add rollbar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary, RollbarContext } from '@rollbar/react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -14,44 +15,57 @@ import useAuth from '../features/auth/authHooks';
 import store from './store';
 import Signup from '../features/signup/Signup';
 
+const rollbarConfig = {
+  accessToken: 'efed73eceb22424cbbb84d88310cd264',
+  environment: 'production',
+};
+
 const App = () => {
   const auth = useAuth();
 
   return (
-    <>
-      <BrowserRouter>
-        <AuthContext.Provider value={auth}>
-          <Provider store={store}>
-            <Switch>
-              <AuthenticatedRoute exact path="/">
-                <Home />
-              </AuthenticatedRoute>
-              <IsNotAuthenticatedRoute path="/login">
-                <Login />
-              </IsNotAuthenticatedRoute>
-              <IsNotAuthenticatedRoute path="/signup">
-                <Signup />
-              </IsNotAuthenticatedRoute>
-              <Route path="*">
-                <NoMatch />
-              </Route>
-            </Switch>
-          </Provider>
-        </AuthContext.Provider>
-      </BrowserRouter>
-      <ToastContainer
-        theme="colored"
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <AuthContext.Provider value={auth}>
+            <Provider store={store}>
+              <Switch>
+                <AuthenticatedRoute exact path="/">
+                  <RollbarContext context="/">
+                    <Home />
+                  </RollbarContext>
+                </AuthenticatedRoute>
+                <IsNotAuthenticatedRoute path="/login">
+                  <RollbarContext context="/login">
+                    <Login />
+                  </RollbarContext>
+                </IsNotAuthenticatedRoute>
+                <IsNotAuthenticatedRoute path="/signup">
+                  <RollbarContext context="/signup">
+                    <Signup />
+                  </RollbarContext>
+                </IsNotAuthenticatedRoute>
+                <Route path="*">
+                  <NoMatch />
+                </Route>
+              </Switch>
+            </Provider>
+          </AuthContext.Provider>
+        </BrowserRouter>
+        <ToastContainer
+          theme="colored"
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
